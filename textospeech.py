062f1f93d0ed1db7014b84b4044e09adc217214e
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-02-23 17:23:58 -0600

    Create textospeech.py

---
+++ textospeech.py
@@ -0,0 +1,11 @@
+#!pip install gtts
+from gtts import gTTS
+
+def tts(text_file, lang, name_file):
+	with open(text_file, "r") as file:
+		text = file.read()
+	file = gTTS(text=text,lang=lang)
+	filename = name_file
+	file.save(filename)
+
+tts("input.txt", "es", "output.mp3")
